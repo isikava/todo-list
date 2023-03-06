a1dace7a2e49450613fff06ded4b287b92482bcb
Alexander Ignatev <alexign473@gmail.com>
2023-03-06 19:49:41 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# todo-list
+# The Odin Project - JavaScript Course
+
+## Project: Todo List
+
+## [Live]()
+
+The Todo List.
+
+## Built with
+
+- HTML
+- CSS
+- Vanilla JS
+- Webpack
+- ESLint + Airbnb JavaScript Style Guide
+- Prettier
+
+## Installation
+
+1. Clone the repo
+
+2. Install the required dependencies by using:  
+   `npm install`
+
+3. Start the Dev server:  
+   `npm start`
+
+4. Distribution files can be produced using:  
+   `npm run build`
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+};
